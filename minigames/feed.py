902b03998e4a7d8d10489138cdee47d813b0713a
Elisk <64747477+eliskk@users.noreply.github.com>
2022-05-11 13:44:22 -0400

    1
    Don't need pygame, there will be a method that renders the array as gfx

--- minigames/feed.py
+++ minigames/feed.py
@@ -9,36 +9,6 @@ EXAMPLE:
 (4/5)
 
 '''
-
-import pygame
-import random
-
-# Constants
-
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
-
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-BROWN = (165,42,42)
-
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Feed the child")
-clock = pygame.time.Clock() 
-
-# Variables
-
-timeleft = 15
-order = []
-
-# Functions
-
 def generate_order(): # test run this
   
   order_num = []
@@ -63,30 +33,3 @@ def generate_order(): # test run this
   
   return order_food
   
-all_sprites = pygame.sprite.group()
-
-running = True
-while running:
-
-    clock.tick(FPS)    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-
-    
-    all_sprites.update()
-
-
-   
-    screen.fill(WHITE)
-
-    
-
-    all_sprites.draw(screen)
-    
-    
-    
-    pygame.display.flip()       
-
-pygame.quit()
