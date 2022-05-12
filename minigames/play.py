4b860565b1779f4810203420150017ec8e76fc52
Elisk <64747477+eliskk@users.noreply.github.com>
2022-05-12 09:25:00 -0400

    Update play.py

--- minigames/play.py
+++ minigames/play.py
@@ -1,3 +1,18 @@
 '''
-idk ill think of something later
+Make the hand game 
 '''
+
+from random import randint
+import mouse
+
+class HandGame:
+	def __init__(self):
+    	self.hit_time = randint(0,7)
+	
+	def warn():
+		return True
+	
+	def hit():
+		if mouse.is_pressed(button='left):
+			if
+			
